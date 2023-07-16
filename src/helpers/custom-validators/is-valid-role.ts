cea3f84611eb995f7eab1d28a41527e7df6e3111
William Reticena <william.wsd43@gmail.com>
2023-07-16 10:57:02 -0300

    feat(custom-validator)> add a custom validator for role

---
+++ src/helpers/custom-validators/is-valid-role.ts
@@ -0,0 +1,31 @@
+import {
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  registerDecorator,
+} from 'class-validator';
+import { CreateUserDto } from '../../dto/create-user.dto';
+
+@ValidatorConstraint({ name: 'isValidRole', async: false })
+export class IsValidRoleConstraint {
+  validate(role: string) {
+    const validRoles = ['Professor', 'Aluno'];
+    return validRoles.includes(role);
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Role ${args.value} is invalid`;
+  }
+}
+
+export function IsValidRole(validationOptions?: ValidationOptions) {
+  return function (object: CreateUserDto, propertyName: string) {
+    registerDecorator({
+      name: 'isValidRole',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: IsValidRoleConstraint,
+    });
+  };
+}
